fbd6dc9f603675d089b0666947f1e2e21cb97d90
Salman Mohammed <sm10793@gmail.com>
2018-08-22 15:31:18 -0700

    Add files via upload

---
+++ Exploration and Prediction.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 30 13:05:29 2017
+
+@author: sal
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from datetime import datetime, date, time, timedelta
+import matplotlib.dates as mdates
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import statsmodels.formula.api as smf
+from sklearn.linear_model import Ridge
+
+
+
+df = pd.read_csv('C:/Users/HP/Desktop/datanonzero.csv')
+
+
+# Describe part
+
+print df.describe()
+
+
+# getting scatter matrix
+
+sns.pairplot(df)
+plt.show()
+
+
+# saving the column values in variables for later use
+
+android_rating_1 = df['android_rating_1']
+android_rating_2 = df['android_rating_2']
+android_rating_3 = df['android_rating_3']
+android_rating_4 = df['android_rating_4']
+android_rating_5 = df['android_rating_5']
+android_total_ratings = df['android_total_ratings']
+ios_all_ratings = df['ios_all_ratings']
+dates = df['dates']
+ios_current_ratings=df['ios_current_ratings']
+
+
+
+# Getting pearson's corr coeffs for the pairs with high correlations
+
+
+print("\n Pearson correlation coeffecients for identified pairs")
+print 65*'_'
+
+
+print("\n The pearson corr coeff between android_total_ratings and ios_all_ratings is {}".format(np.corrcoef(ios_all_ratings,android_total_ratings)[0][1]))
+print("\n The pearson corr coeff between android_total_ratings and android_rating_1 is {}".format(np.corrcoef(android_rating_1,android_total_ratings)[0][1]))
+print(" The pearson corr coeff between android_total_ratings and android_rating_2 is {}".format(np.corrcoef(android_rating_2,android_total_ratings)[0][1]))
+print(" The pearson corr coeff between android_total_ratings and android_rating_3 is {}".format(np.corrcoef(android_rating_3,android_total_ratings)[0][1]))
+print(" The pearson corr coeff between android_total_ratings and android_rating_4 is {}".format(np.corrcoef(android_rating_4,android_total_ratings)[0][1]))
+print(" The pearson corr coeff between android_total_ratings and android_rating_5 is {}".format(np.corrcoef(android_rating_5,android_total_ratings)[0][1]))
+print(" \n The pearson corr coeff between android_rating_1 and ios_all_ratings is {}".format(np.corrcoef(ios_all_ratings,android_rating_1)[0][1]))
+print(" The pearson corr coeff between android_rating_2 and ios_all_ratings is {}".format(np.corrcoef(ios_all_ratings,android_rating_2)[0][1]))
+print(" The pearson corr coeff between android_rating_3 and ios_all_ratings is {}".format(np.corrcoef(ios_all_ratings,android_rating_3)[0][1]))
+print(" The pearson corr coeff between android_rating_4 and ios_all_ratings is {}".format(np.corrcoef(ios_all_ratings,android_rating_4)[0][1]))
+print(" The pearson corr coeff between android_rating_5 and ios_all_ratings is {}".format(np.corrcoef(ios_all_ratings,android_rating_5)[0][1]))
+print
+print("Checking for multicollinearity")
+print 65*'_'
+print(" The pearson corr coeff between android_rating_2 and android_rating_1 is {}".format(np.corrcoef(android_rating_2,android_rating_1)[0][1]))
+print(" The pearson corr coeff between android_rating_3 and android_rating_1 is {}".format(np.corrcoef(android_rating_3,android_rating_1)[0][1]))
+print(" The pearson corr coeff between android_rating_4 and android_rating_1 is {}".format(np.corrcoef(android_rating_4,android_rating_1)[0][1]))
+print(" The pearson corr coeff between android_rating_5 and android_rating_1 is {}".format(np.corrcoef(android_rating_5,android_rating_1)[0][1]))
+
+
+print 65*'_'
+print(" The pearson corr coeff between android_rating_1 and android_rating_2 is {}".format(np.corrcoef(android_rating_1,android_rating_2)[0][1]))
+print(" The pearson corr coeff between android_rating_3 and android_rating_2 is {}".format(np.corrcoef(android_rating_3,android_rating_2)[0][1]))
+print(" The pearson corr coeff between android_rating_4 and android_rating_2 is {}".format(np.corrcoef(android_rating_4,android_rating_2)[0][1]))
+print(" The pearson corr coeff between android_rating_5 and android_rating_2 is {}".format(np.corrcoef(android_rating_5,android_rating_2)[0][1]))
+
+
+print 65*'_'
+print(" The pearson corr coeff between android_rating_1 and android_rating_3 is {}".format(np.corrcoef(android_rating_1,android_rating_3)[0][1]))
+print(" The pearson corr coeff between android_rating_2 and android_rating_3 is {}".format(np.corrcoef(android_rating_2,android_rating_3)[0][1]))
+print(" The pearson corr coeff between android_rating_4 and android_rating_3 is {}".format(np.corrcoef(android_rating_4,android_rating_3)[0][1]))
+print(" The pearson corr coeff between android_rating_5 and android_rating_3 is {}".format(np.corrcoef(android_rating_5,android_rating_3)[0][1]))
+
+
+print 65*'_'
+print(" The pearson corr coeff between android_rating_1 and android_rating_4 is {}".format(np.corrcoef(android_rating_1,android_rating_4)[0][1]))
+print(" The pearson corr coeff between android_rating_2 and android_rating_4 is {}".format(np.corrcoef(android_rating_2,android_rating_4)[0][1]))
+print(" The pearson corr coeff between android_rating_3 and android_rating_4 is {}".format(np.corrcoef(android_rating_3,android_rating_4)[0][1]))
+print(" The pearson corr coeff between android_rating_5 and android_rating_4 is {}".format(np.corrcoef(android_rating_5,android_rating_4)[0][1]))
+
+
+print 65*'_'
+print(" The pearson corr coeff between android_rating_1 and android_rating_5 is {}".format(np.corrcoef(android_rating_1,android_rating_5)[0][1]))
+print(" The pearson corr coeff between android_rating_2 and android_rating_5 is {}".format(np.corrcoef(android_rating_2,android_rating_5)[0][1]))
+print(" The pearson corr coeff between android_rating_3 and android_rating_5 is {}".format(np.corrcoef(android_rating_3,android_rating_5)[0][1]))
+print(" The pearson corr coeff between android_rating_4 and android_rating_5 is {}".format(np.corrcoef(android_rating_4,android_rating_5)[0][1]))
+
+"""
+
+
+# plotting time series graphs 
+
+start= datetime(2016,7,21,0,0)
+end= datetime(2016,11,1,0,0)
+time_delta=timedelta(minutes=10)
+
+x=[]
+while start<end:
+    x.append(start)
+    start=start+time_delta
+
+x_axis=np.array(x)
+t=np.array(dates)
+y = np.array(android_total_ratings)
+print x_axis.shape
+print y.shape
+print t.shape
+print len(x)
+fig, ax = plt.subplots(1)
+fig.autofmt_xdate()
+plt.plot(t,y)
+xfmt = mdates.DateFormatter('%d-%m-%y %H:%M')
+ax.xaxis.set_major_formatter(xfmt)
+plt.show()
+
+#plt.plot(x,y)
+
+times = pd.date_range('2016-10-06', periods=500, freq='10min')
+fig, ax = plt.subplots(1)
+fig.autofmt_xdate()
+plt.plot(times, range(times.size))
+xfmt = mdates.DateFormatter('%d-%m-%y %H:%M')
+ax.xaxis.set_major_formatter(xfmt)
+
+plt.show()
+"""
+
+
+# getting prediction model for android
+
+print("\n simple linear regression for android_total_ratings")
+print 65*'_'
+model1 = LinearRegression(normalize=True)
+print('\n')
+print(model1)
+x= np.array(android_rating_5)
+y= np.array(android_total_ratings)
+
+
+X = x[:, None] # Transposing
+
+#print(X)
+#print(y) 
+
+#print X.shape
+
+model1.fit(X, y)
+print('\nthe model coefficient for android model is {}'.format(model1.coef_))
+print('the model intercept for android model is {}'.format(model1.intercept_))
+#print (model1.residues_)
+print ("The model's prediction for android_all_ratings for the given feature values is {} ".format(model1.predict([[726597],[732956],[4237996], [4239138]]))) # for the first and last value in the csv file
+print("The R squared value for the model is {}".format(model1.score(X,y)))
+
+
+
+
+# getting prediction model for ios
+
+print("\n simple linear regression for ios_all_ratings")
+print 65*'_'
+model2 = LinearRegression(normalize=True)
+print('\n')
+print(model2)
+x= np.array(android_rating_5)
+y= np.array(ios_all_ratings)
+
+
+X = x[:, None] # Transposing
+#print(X)
+#print(y) 
+#print X.shape
+
+model2.fit(X, y)
+print('\nthe model coefficient for ios model is {}'.format(model2.coef_))
+print('the model intercept for ios model is {}'.format(model2.intercept_))
+#print (model2.residues_)
+print ("The model's prediction for ios_all_ratings for the given feature values is {} ".format(model2.predict([[726597], [4239138]]))) # for the first and last value in the csv file
+print("The R squared value for the model is {}".format(model2.score(X,y)))
+print
+
+
+
+
+#  multiple linear regression for android total ratings
+print("\n multiple linear regression for android_total_ratings")
+print 65*'_'
+feature_cols = ['android_rating_5', 'android_rating_4','android_rating_3','android_rating_2']
+X = df[feature_cols]
+y = df.android_total_ratings
+lm = LinearRegression()
+
+# cross validation for android model
+
+print("\nCross validation for android model")
+print 65*'_'
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
+lm.fit(Xtrain, ytrain)
+ypred = lm.predict(Xtest)
+print('the model intercept for android multiple linear regression model is {}'.format(lm.intercept_))
+print('\nthe model coefficient for android multple linear regression is {}'.format(lm.coef_))
+print ("The model's prediction for android_total_ratings for the given feature values is {} ".format(ypred)) 
+print("The R squared value for the model is {}".format(lm.score(Xtrain,ytrain)))
+print
+print("The root mean sqaured error is {}".format(sqrt(mean_squared_error(ytest, ypred))))
+
+
+
+
+# Trying multiple linear regression for ios total ratings
+print("\n multiple linear regression for ios_all_ratings")
+print 65*'_'
+feature_cols = ['android_rating_5', 'android_rating_4','android_rating_3','android_rating_2']
+X = df[feature_cols]
+y = df.ios_all_ratings
+
+lm = LinearRegression()
+#lm.fit(X, y)
+
+
+
+# cross validation on ios model
+
+print("\nCross validation for android model")
+print 65*'_'
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
+lm.fit(Xtrain, ytrain)
+ypred = lm.predict(Xtest)
+print('the model intercept for ios multiple linear regression model is {}'.format(lm.intercept_))
+print('\nthe model coefficient for ios multple linear regression is {}'.format(lm.coef_))
+print ("The model's prediction for ios_all_ratings for the given feature values is {} ".format(ypred)) 
+print("The R squared value for the model is {}".format(lm.score(Xtrain,ytrain)))
+print()
+print("The root mean sqaured error is {}".format(sqrt(mean_squared_error(ytest, ypred))))
+
+
+
+# regression using statsmodel
+
+print("\n\nRegression using statsmodel")
+print 65*'_'
+
+result = smf.ols(formula='android_total_ratings ~ android_rating_5 +android_rating_5+ ios_all_ratings   ' , data=df).fit()
+print
+print result.params
+print result.summary()
+# print the coefficients 4135326	6637612	224252
+
+# our prediction
+print
+print("\n OUR PREDICTIONS")
+print 65*'_'
+print
+print(" IOS MODEL")
+print 65*'_'
+feature_cols = ['android_rating_5']
+X = df[feature_cols]
+y = df.ios_all_ratings
+
+lm = LinearRegression()
+lm.fit(X, y)
+
+# print intercept and coefficients
+print ("\nThe intercept is : {}".format(lm.intercept_))
+print ("\nThe coeffecients are: {}".format(zip(feature_cols,lm.coef_)))
+print (" \nOur prediction for the ios all rating is : {}".format(lm.predict([4239138])))
+
+print
+print
+print(" ANDROID MODEL")
+print 65*'_'
+feature_cols = ['android_rating_5','ios_all_ratings','android_rating_1','android_rating_3']
+X = df[feature_cols]
+y = df.android_total_ratings
+
+lm = LinearRegression()
+lm.fit(X, y)
+
+print ("\nThe intercept is : {}".format(lm.intercept_))
+print ("\nThe coeffecients are: {}".format(zip(feature_cols,lm.coef_)))
+print (" \nOur prediction for the android_total_ratings is : {}".format(lm.predict([4239138,230601,952604,514286,])))
+print
+print 
---
+++ Web scraping and data organization.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+import glob
+from bs4 import BeautifulSoup
+import datetime
+import os
+import gc
+from pprint import pprint
+import pandas as pd
+from pandas import ExcelWriter
+
+
+
+gc.enable()
+data_dict=dict()
+final_dict=dict()
+
+
+os.chdir("C:/Users/Salman Mohammed/Desktop/Sem2 Spring 2017/1) Data Science INSY-5378-001/PROJECTS/Project 2/WD/data")
+
+for folders in glob.iglob('*'):
+    fol_date = datetime.datetime.strptime(folders, '%Y-%m-%d')
+    min_counter = 0
+    time_delta = datetime.timedelta(minutes=min_counter)
+    #os.chdir("C:/Users/Salman Mohammed/Desktop/Sem2 Spring 2017/1) Data Science INSY-5378-001/PROJECTS/Project 2/pokemon_5378 (2)/data/" + folders)
+    os.chdir("C:/Users/Salman Mohammed/Desktop/Sem2 Spring 2017/1) Data Science INSY-5378-001/PROJECTS/Project 2/WD/data/" + folders)
+    print 'next folder'
+    for filename in glob.iglob('*.html'):
+        min_counter = 10
+        time_delta = datetime.timedelta(minutes=min_counter)
+        print filename
+        with open(filename) as f:
+            
+            if 'android' in filename:
+                print 'in android'
+                
+                soupA = BeautifulSoup(f , 'lxml' )
+                # getting android star ratings
+                star_ratings = soupA.find_all("span",{"class":"bar-number"})
+                android_ratings_5 = star_ratings[0].get_text()
+                android_ratings_4 = star_ratings[1].get_text()
+                android_ratings_3 = star_ratings[2].get_text()
+                android_ratings_2 = star_ratings[3].get_text()
+                android_ratings_1 = star_ratings[4].get_text()
+                
+                #  getting total ratings
+                tot_ratings = soupA.find_all("span",{"class":"reviews-num"})
+                tot_ratings_value = tot_ratings[0].get_text()
+                
+                # getting average rating
+                avg_rating = soupA.find_all("div",{"class":"score"})
+                avg_rating_value = avg_rating[0].get_text()
+                
+                # getting file size
+                file_size = soupA.find_all("div",{"itemprop":"fileSize"})
+                file_size_value= file_size[0].get_text()
+
+            elif 'ios' in filename:
+                print 'in ios'
+                soupI = BeautifulSoup(f , 'lxml' )
+                # getting ratings from android html files
+                Customer_Ratings = soupI.find_all('span' , {'class':'rating-count'})
+                ios_current_ratings= Customer_Ratings[0].get_text().split(" ")[0]
+                ios_all_ratings= Customer_Ratings[1].get_text().split(" ")[0]
+                
+                # getting file size from android html files
+                File_Size = soupI.find_all('span' , {'class':'label'})
+                ios_file_size= File_Size[3].parent.get_text().split(" ")[1]
+                
+                # putting data into dictionary
+                final_dict[fol_date]={}
+                    # ios data
+                final_dict[fol_date]['ios_current_ratings']=ios_current_ratings
+                final_dict[fol_date]['ios_all_ratings']=ios_all_ratings
+                final_dict[fol_date]['ios_file_size']=ios_file_size
+                    # Android data
+                final_dict[fol_date]['android_avg_rating']=avg_rating_value
+                final_dict[fol_date]['android_total_ratings']=tot_ratings_value
+                final_dict[fol_date]['android_rating_1']=android_ratings_1
+                final_dict[fol_date]['android_rating_2']=android_ratings_2
+                final_dict[fol_date]['android_rating_3']=android_ratings_3
+                final_dict[fol_date]['android_rating_4']=android_ratings_4   
+                final_dict[fol_date]['android_rating_5']=android_ratings_5
+                final_dict[fol_date]['android_file_size']=file_size_value
+        
+                pprint(final_dict)
+                fol_date=fol_date+time_delta   
+                print fol_date
+
+print "making Pandas"
+print final_dict.keys()
+Pokemon_dataframe = pd.DataFrame(final_dict)
+Pokemon_dataframe= Pokemon_dataframe.transpose()
+print Pokemon_dataframe 
+
+os.chdir("C:/Users/Salman Mohammed/Desktop/Sem2 Spring 2017/1) Data Science INSY-5378-001/PROJECTS/Project 2/WD")
+
+with open('data.json' ,'w') as wjf:
+    wjf.write(Pokemon_dataframe.to_json())
+    
+with open('data.csv' ,'w') as wcf:
+    wcf.write(Pokemon_dataframe.to_csv())
+
+writer = ExcelWriter('data.xlsx')
+Pokemon_dataframe.to_excel(writer,'Sheet1')
+writer.save()
+
+
